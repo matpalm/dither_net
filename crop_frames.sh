081f021feb35f83c9ee2ebb02b1066eea6088b05
Mat Kelcey <matthew.kelcey@gmail.com>
2020-10-21 20:54:46 +1100

    cull some old stuff

--- crop_frames.sh
+++
@@ -1,9 +0,0 @@
-
-set -e
-rm frames/$1/f_00000*.jpg
-rm frames/$1/f_00001*.jpg
-rm frames/$1/f_00002*.jpg
-rm frames/$1/f_00163*.jpg
-rm frames/$1/f_00164*.jpg
-rm frames/$1/f_00165*.jpg
-rm frames/$1/f_00166*.jpg
